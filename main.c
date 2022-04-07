e84f6f8c2bca25c4ac1a2c57e3cb82a08c04ac8c
Mohamed Abdullsalam <70759290+Mohammed4mach@users.noreply.github.com>
2022-04-07 10:21:21 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  char line[150];
+  
+  FILE * pf;
+  pf = fopen("file.txt", "r");
+  
+  fgets(line, 150, pf);
+  printf("String in the file: %s", line);
+  
+  for(int i = 0, j; line[i] != '\0'; i++)
+  {
+    if(!(line[i] >= 'a' && line[i] <= 'z') && !(line[i] >= 'A' && line[i] <= 'Z') && line[i] != '\0')
+    {
+      for(j = i; line[j] != '\0'; ++j)
+      {
+        line[j] = line[j + 1];
+      }
+      line[j] = '\0';
+    }
+  }
+  printf("\nOutput string: ");
+  puts(line);
+  gets(line);
+  
+  return 0;
+}
